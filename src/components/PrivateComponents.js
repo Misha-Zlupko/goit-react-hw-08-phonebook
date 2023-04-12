8738628b5f74e5be6049e4285861cfd8ec419d44
mafioz359 <64733979+mafioz359@users.noreply.github.com>
2023-04-12 21:47:01 +0300

    в

--- src/components/PrivateComponents.js
+++ src/components/PrivateComponents.js
@@ -1,7 +1,20 @@
-import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+// import { Navigate } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
 
-export const PrivateRoute = ({ children, redirectTo = '/' }) => {
-  const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
-  return isLoggedIn ? children : <Navigate to={redirectTo} />;
+// export const PrivateRoute = ({ children, redirectTo = '/' }) => {
+//   const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+//   return isLoggedIn ? children : <Navigate to={redirectTo} />;
+// };
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { selectIsLoggedIn } from '../redux/auth/selectors';
+import { useSelector } from 'react-redux';
+export const PrivateRoute = ({ children }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  if (!isLoggedIn) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
 };
--- src/components/RestrictedRoute.js
+++ src/components/RestrictedRoute.js
@@ -1,9 +1,21 @@
-import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { selectIsLoggedIn } from 'redux/auth/selectors';
+// import { Navigate } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
+// import { selectIsLoggedIn } from 'redux/auth/selectors';
 
-export const PublicRoute = ({ children, redirectTo = '/' }) => {
+// export const PublicRoute = ({ children, redirectTo = '/' }) => {
+//   const isLoggedIn = useSelector(selectIsLoggedIn);
+//   const redirect = isLoggedIn;
+//   return redirect ? <Navigate to={redirectTo} /> : children;
+// };
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../redux/auth/selectors';
+import { Navigate } from 'react-router-dom';
+export const PublicRoute = ({ children }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
-  const redirect = isLoggedIn;
-  return redirect ? <Navigate to={redirectTo} /> : children;
+
+  if (isLoggedIn) {
+    return <Navigate to={'/'} />;
+  }
+  return children;
 };
